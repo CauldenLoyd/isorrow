237eb92fb19e81a75e8c5f49eb726662f2244c46
Caulden Loyd <97518430+CauldenLoyd@users.noreply.github.com>
2022-01-12 01:41:43 -0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Caulden Loyd!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Coming Soon" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Caulden's Portfolio
         </p>
       </main>
 
